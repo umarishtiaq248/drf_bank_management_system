dc274fe2f82e02b1c27a927117d2b620a9b67ebb
umarishtiaq248 <umarishtiaq63@gmail.com>
2025-02-19 15:31:50 +0500

    Bank model is created,Rest framework is added in settings

--- banking_app/models.py
+++ banking_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Bank(models.Model):
+    bank_name = models.CharField(max_length=128)
+    branch_name = models.CharField(max_length=128)
+    is_islamic = models.BooleanField(null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.bank_name
