ef662565cd8815e29a8b1417f92daa703175b57b
umarishtiaq248 <umarishtiaq63@gmail.com>
2025-02-21 16:49:06 +0500

    view is created as bank_view function

--- banking_app/views.py
+++ banking_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import  status
+from .serializers import BankSerializer
+from .models import Bank
 
-# Create your views here.
+@api_view(['GET','POST'])
+def bank_view(request):
+    if request.method=='GET':
+        bank = Bank.object.all()
+        serializer = BankSerializer(bank,many=True)
+        return Response(serializer.data)
+    elif request.method=="POST":
+        serializer = BankSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
