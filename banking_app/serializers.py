ba3e78dc3c6fd88c32b21932acdab203693ec612
umarishtiaq248 <umarishtiaq63@gmail.com>
2025-02-24 17:14:26 +0500

    Account model serializer is created

--- banking_app/serializers.py
+++ banking_app/serializers.py
@@ -1,7 +1,12 @@
 from  rest_framework import serializers
-from .models import Bank
+from .models import Bank, Account
 
 class BankSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bank
+        fields = "__all__"
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
         fields = "__all__"
