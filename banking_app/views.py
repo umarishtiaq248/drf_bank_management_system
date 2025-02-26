c7d008486e2d12bd4040308965b4f9652c29ed78
umarishtiaq248 <umarishtiaq63@gmail.com>
2025-02-26 15:40:31 +0500

    bank and account api is created by using generic apiview

--- banking_app/views.py
+++ banking_app/views.py
@@ -1,14 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from  rest_framework.permissions import AllowAny
-from rest_framework import  viewsets
+from rest_framework import  viewsets,generics
 from .serializers import BankSerializer,AccountSerializer
 from .models import Bank,Account
 
 class BankListApiView(APIView):
     permission_classes = [AllowAny]
     http_method_names = ['get']
-    def get(self,request):
+
+    def get(self, request, *args, **kwargs):
         bank_qs = Bank.objects.all()
         serializer = BankSerializer(bank_qs,many=True)
         return Response(serializer.data)
@@ -16,8 +17,9 @@ class BankListApiView(APIView):
 class AccountListApiView(APIView):
     permission_classes = [AllowAny]
     http_method_names = ['get']
-    def get(self,request):
-        user_name = request.GET.get('name','')
+
+    def get(self, request, *args, **kwargs):
+        user_name = request.GET.get('name')
         if user_name:
             account_qs = Account.objects.filter(user_name__icontains=user_name)
             serializer = AccountSerializer(account_qs, many=True)
@@ -41,3 +43,23 @@ class AccountListViewSet(viewsets.ModelViewSet):
             return Account.objects.filter(user_name__icontains=user_name)
         else:
             return Response({"message": "No user name provided"}, status=400)
+
+class BankListGenericApiview(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    http_method_names = ['get']
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+
+class AccountListGenericApiview(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    http_method_names = ['get']
+    serializer_class = AccountSerializer
+    def get_queryset(self):
+        user_name = self.request.GET.get('name')
+        if user_name:
+            return Account.objects.filter(user_name__icontains=user_name)
+    def get(self, request, *args, **kwargs):
+        user_name = request.GET.get('name')
+        if not user_name:
+            return Response({"message": "No user name provided"})
+        return super().get(request, *args, **kwargs)
