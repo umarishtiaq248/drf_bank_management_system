64978deeb5d3a18021b51c0a505b2a4a60cd0fda
umarishtiaq248 <umarishtiaq63@gmail.com>
2025-02-26 12:06:01 +0500

    APIview is added again and api is created for every view in views.py

--- banking_app/urls.py
+++ banking_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path,include
-from .views import BankListApiView,AccountListApiView
+from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('bank-list',BankListApiView,basename='bank_list')
-router.register('account-list',AccountListApiView,basename='account_list')
+router.register('bank-list/viewset',views.BankListViewSet,basename='bank_list-viewset')
+router.register('account-list/viewset',views.AccountListViewSet,basename='account_list-viewset')
 
 urlpatterns = [
     path('api/',include(router.urls)),
+    path('api/bank-list/apiview/',views.BankListApiView.as_view(),name='bank_list-apiview'),
+    path('api/account-list/apiview/',views.AccountListApiView.as_view(),name='account_list-apiview')
 ]
--- banking_app/views.py
+++ banking_app/views.py
@@ -5,13 +5,31 @@ from rest_framework import  viewsets
 from .serializers import BankSerializer,AccountSerializer
 from .models import Bank,Account
 
-class BankListApiView(viewsets.ModelViewSet):
+class BankListApiView(APIView):
+    permission_classes = [AllowAny]
+    http_method_names = ['get']
+    def get(self,request):
+        bank_qs = Bank.objects.all()
+        serializer = BankSerializer(bank_qs,many=True)
+        return Response(serializer.data)
+
+class AccountListApiView(APIView):
+    permission_classes = [AllowAny]
+    http_method_names = ['get']
+    def get(self,request):
+        user_name = request.GET.get('name','')
+        if user_name:
+            account_qs = Account.objects.filter(user_name__icontains=user_name)
+            serializer = AccountSerializer(account_qs, many=True)
+            return Response(serializer.data)
+
+class BankListViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     http_method_names = ['get']
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
-class AccountListApiView(viewsets.ModelViewSet):
+class AccountListViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     http_method_names = ['get']
     serializer_class = AccountSerializer
