71a206bd921842b16fa0ff270b64f64b358369bf
umarishtiaq248 <umarishtiaq63@gmail.com>
2025-02-26 16:37:37 +0500

    extra attributes is removed,Searchfilter is used in GenericApiview

--- banking_app/views.py
+++ banking_app/views.py
@@ -1,65 +1,47 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from  rest_framework.permissions import AllowAny
-from rest_framework import  viewsets,generics
+from rest_framework import  viewsets,generics,filters
 from .serializers import BankSerializer,AccountSerializer
 from .models import Bank,Account
 
 class BankListApiView(APIView):
-    permission_classes = [AllowAny]
-    http_method_names = ['get']
-
-    def get(self, request, *args, **kwargs):
+    def get (self,request):
         bank_qs = Bank.objects.all()
         serializer = BankSerializer(bank_qs,many=True)
         return Response(serializer.data)
 
 class AccountListApiView(APIView):
-    permission_classes = [AllowAny]
-    http_method_names = ['get']
-
-    def get(self, request, *args, **kwargs):
-        user_name = request.GET.get('name')
+    def get(self, request):
+        queryset = Account.objects.all()
+        user_name = self.request.query_params.get('name')
         if user_name:
-            account_qs = Account.objects.filter(user_name__icontains=user_name)
+            account_qs = queryset.filter(user_name__icontains=user_name)
             serializer = AccountSerializer(account_qs, many=True)
             return Response(serializer.data)
         else:
-            return Response({"message": "No user name provided"}, status=400)
+            serializer = AccountSerializer(queryset, many=True)
+            return Response(serializer.data)
 
 class BankListViewSet(viewsets.ModelViewSet):
-    permission_classes = [AllowAny]
-    http_method_names = ['get']
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
 class AccountListViewSet(viewsets.ModelViewSet):
-    permission_classes = [AllowAny]
-    http_method_names = ['get']
     serializer_class = AccountSerializer
+    queryset = Account.objects.all()
     def get_queryset(self):
-        user_name = self.request.GET.get('name')
+        user_name = self.request.query_params.get('name')
         if user_name:
-            return Account.objects.filter(user_name__icontains=user_name)
+            return self.queryset.filter(user_name__icontains=user_name)
         else:
-            return Response({"message": "No user name provided"}, status=400)
+            return self.queryset
 
 class BankListGenericApiview(generics.ListAPIView):
-    permission_classes = [AllowAny]
-    http_method_names = ['get']
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
 class AccountListGenericApiview(generics.ListAPIView):
-    permission_classes = [AllowAny]
-    http_method_names = ['get']
+    queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    def get_queryset(self):
-        user_name = self.request.GET.get('name')
-        if user_name:
-            return Account.objects.filter(user_name__icontains=user_name)
-    def get(self, request, *args, **kwargs):
-        user_name = request.GET.get('name')
-        if not user_name:
-            return Response({"message": "No user name provided"})
-        return super().get(request, *args, **kwargs)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user_name']
